92657d9eabcf474127c7d11cb9f6f6c062831ace
Pavlos Karagiorgos <pavloskaragiorgos@gmail.com>
2026-07-12 15:10:54 +0300

    Deleted the duplicate strategy

--- WorldRank.Application/Strategies/ForceSubtractFundsStrategy.cs
+++ WorldRank.Application/Strategies/ForceSubtractFundsStrategy.cs
@@ -1,9 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using WorldRank.Application.Strategies;
 using WorldRank.Domain.Entities;
 
-namespace WorldRank.Application.Strategies;
-
-public class ForceSubtractFundsStrategy : IFundsStrategy
+namespace WorldRank.Application.Strategies
 {
-    public FundsOperation Operation => FundsOperation.ForceSubtract;
-    public void Execute(Wallet wallet, decimal amount) => wallet.ForceWithdraw(amount);
+    public class ForceSubtractFundsStrategy : IFundsStrategy
+    {
+        public FundsOperation Operation => FundsOperation.ForceSubtract;
+        public void Execute(Wallet wallet, decimal amount) => wallet.ForceWithdraw(amount);
+    }
 }
